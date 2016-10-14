91476f154070392d097663c0155bc4f2c911342a
Marcos Orozco <dexter_mister@hotmail.com>
2016-10-14 16:41:09 -0600

    Funcionando contenedor de contenido

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -2,7 +2,7 @@
 
 @section('menu') 
   <div class="container">
-<<<<<<< HEAD
+
     <div class="row">
       <ul class="ulu">
         <li class="lis"><a class="as" href="#">Home</a></li>
@@ -12,19 +12,6 @@
         <li class="lis"><a class="as" href="#">Fuerza</a></li>
       </ul>  
     </div>
-=======
-  <div class="row">
-    
-        <ul class="ulu">
-  <li class="lis"><a class="as" href="#">Home</a></li>
-  <li class="lis"><a class="as" href="#">Pokedex</a></li>
-  <li class="lis"><a class="as" href="#">Tipos</a></li>
-  <li class="lis"><a class="as" href="#">Generaciones</a></li>
-  <li class="lis"><a class="as" href="#">Fuerza</a></li>
-</ul>
-        
->>>>>>> f79773c9afb3196986108cae769be738966f6d95
-  </div>
 @stop
 
 @section('content')
