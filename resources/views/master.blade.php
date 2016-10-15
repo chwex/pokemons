abb72ea62ebc3bad435cc7bf447b47869b65a256
Carlos Valdez <Carlos Valdez>
2016-10-14 19:01:31 -0600

    modificaciones carlos

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,21 +8,12 @@
     <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset("css/principal.css")}}">
-    <title>Simple Sidebar - Start Bootstrap Template</title>
+    <title>Pokemons Digital Mosnters</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="css/simple-sidebar.css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
 
 <body>
@@ -54,19 +45,6 @@
         </div>
     </div>    
 </section>
-    
-        <!-- /#page-content-wrapper -->
-
-
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Menu Toggle Script -->
 
 </body>
 
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,49 +1,11 @@
 @extends('layouts.base')
-
 <!-- Bootstrap Core JavaScript -->
-<script src="lib/js/bootstrap.min.js"></script>
+
 <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="{{asset("//css/principal.css")}}">
-</head>
-<body>
 
-<section class="container">
-	<div class="row">
-		<nav class="navbar navbar-default">
-  			<div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-  				<div class="navbar-header">
-  					<a href="{{url('/master')}}"><img  src="English_Pok.png" alt="pokemon icon"></a>
-<!--<a class="navbar-brand" href="{{url('/master')}}">Pokemons</a>-->
-  				</div>
-    			<form class="navbar-form navbar-right">
-    				<div class="form-group">
-          				<input type="text" class="form-control" placeholder="Buscar">
-          				<input type="button" class="imagenboton" name="">
-        			</div>		
-      			</form>
-  			</div><!-- /.container-fluid -->
-		</nav>
-	</div>
-</section>
-<section class="container-fluid">
-  <div class="container">
-
-    <div class="row">
-      <ul class="ulu">
-        <li class="lis"><a class="as" href="#">Home</a></li>
-        <li class="lis"><a class="as" href="#">Pokedex</a></li>
-        <li class="lis"><a class="as" href="{{url('/tipos')}}">Tipos</a></li>
-        <li class="lis"><a class="as" href="#">Generaciones</a></li>
-        <li class="lis"><a class="as" href="#">Fuerza</a></li>
-      </ul>  
-    </div>
-@stop
 
 @section('content')
 
 <p><center>AQUI SE PONDRA TODO EL CONTENIDO MA FRENDS</center></p>
 
 @stop
-
