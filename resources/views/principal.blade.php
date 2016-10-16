1d4e7f7d4cf7cb8b95b2c59f48848fe36fc22d68
david orlando hernandez bastida <david_kook@hotmail.com>
2016-10-15 23:51:43 -0600

    Otro ejemplo de commit

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<p><center>AQUI SE PONDRA TODO EL CONTENIDO MA FRENDS, SOLO CALANDO COMMIT</center></p>
+<p><center>AQUI SE PONDRA TODO EL CONTENIDO MA FRENDS, SOLO CALANDO COMMIT otra vez </center></p>
 
 
 @stop
